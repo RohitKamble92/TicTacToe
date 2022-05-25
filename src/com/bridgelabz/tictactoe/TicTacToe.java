7ec4fa78a9c9edd1fc24f2b11c9c5b3538f107f0
Rohit <rohitkamble10792@gmail.com>
2022-05-25 08:55:41 +0530

    InitializeBoard

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,7 +1,22 @@
 package com.bridgelabz.tictactoe;
 
 public class TicTacToe {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tac Toe Game Program");
+    private static char[] boardArray= new char[10];
+    public static void initBoard(){
+        for(int i=1;i<10;i++)
+        {
+            boardArray[i]=' ';
+        }
     }
-}
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Tic Tac Toe Game Program");
+
+
+            }
+        }
+
+
+
+
